480cb8bb30455c22dcb79a2d27c0c0364d485930
Breno Santana <breno.lima@sou.inteli.edu.br>
2025-10-02 10:06:41 -0300

    feat: Response entity in user route

--- src/backend/socialnetwork/src/main/java/com/meuapp/socialnetwork/resources/UserResource.java
+++ src/backend/socialnetwork/src/main/java/com/meuapp/socialnetwork/resources/UserResource.java
@@ -11,6 +11,7 @@ import com.meuapp.socialnetwork.domain.User;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 
 
@@ -20,11 +21,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class UserResource {
  
     @RequestMapping(method=RequestMethod.GET)
-    public List<User> findAll() {
+    public ResponseEntity<List<User>> findAll() {
         User maria = new User("1", "Maria", "maria@gmail.com");
         User alex = new User("2", "Alex", "alex@gmail.com");
         List<User> list = new ArrayList<>();
         list.addAll(Arrays.asList(maria, alex));
-        return list;
+        return ResponseEntity.ok().body(list);
     }
 }
