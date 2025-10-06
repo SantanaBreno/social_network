11a2b3f7ddcf013af9b8b7e0f33b5501f8da9183
Breno Santana <breno.lima@sou.inteli.edu.br>
2025-10-06 11:45:29 -0300

    feat: add UserDTO

--- src/backend/socialnetwork/src/main/java/com/meuapp/socialnetwork/resources/UserResource.java
+++ src/backend/socialnetwork/src/main/java/com/meuapp/socialnetwork/resources/UserResource.java
@@ -3,10 +3,12 @@ package com.meuapp.socialnetwork.resources;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.web.bind.annotation.RestController;
 
 import com.meuapp.socialnetwork.domain.User;
+import com.meuapp.socialnetwork.dto.UserDTO;
 import com.meuapp.socialnetwork.services.UserService;
 
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,8 +28,9 @@ public class UserResource {
     private UserService service;
 
     @RequestMapping(method=RequestMethod.GET)
-    public ResponseEntity<List<User>> findAll() {
+    public ResponseEntity<List<UserDTO>> findAll() {
         List<User> list = service.findAll();
-        return ResponseEntity.ok().body(list);
+        List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+        return ResponseEntity.ok().body(listDto);
     }
 }
