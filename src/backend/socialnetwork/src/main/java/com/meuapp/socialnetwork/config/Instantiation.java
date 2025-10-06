054e7bb60f781e857c173580875517662623e42b
Breno Santana <breno.lima@sou.inteli.edu.br>
2025-10-06 11:17:29 -0300

    feat: add Instantiation mongodb

---
+++ src/backend/socialnetwork/src/main/java/com/meuapp/socialnetwork/config/Instantiation.java
@@ -0,0 +1,30 @@
+package com.meuapp.socialnetwork.config;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import com.meuapp.socialnetwork.domain.User;
+import com.meuapp.socialnetwork.repository.UserRepository;
+
+@Configuration
+public class Instantiation implements CommandLineRunner {
+    
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public void run(String... arg0) throws Exception {
+        
+        userRepository.deleteAll();
+
+        User maria = new User(null, "Maria Brown", "maria@gmail.com");
+        User alex = new User(null, "Alex Green", "alex@gmail.com");
+        User bob = new User(null, "Bob Grey", "bob@gmail.com");
+
+        userRepository.saveAll(Arrays.asList(maria, alex, bob));
+    }
+}
