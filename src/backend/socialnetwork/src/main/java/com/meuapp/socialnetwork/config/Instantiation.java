76d728d28df8a50a6659f9196f68de6ea1dd4e36
Breno Santana <breno.lima@sou.inteli.edu.br>
2025-10-21 18:09:38 -0300

    feat: AuthorDTO

--- src/backend/socialnetwork/src/main/java/com/meuapp/socialnetwork/config/Instantiation.java
+++ src/backend/socialnetwork/src/main/java/com/meuapp/socialnetwork/config/Instantiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.meuapp.socialnetwork.domain.Post;
 import com.meuapp.socialnetwork.domain.User;
+import com.meuapp.socialnetwork.dto.AuthorDTO;
 import com.meuapp.socialnetwork.repository.PostRepository;
 import com.meuapp.socialnetwork.repository.UserRepository;
 
@@ -36,10 +37,11 @@ public class Instantiation implements CommandLineRunner {
         User alex = new User(null, "Alex Green", "alex@gmail.com");
         User bob = new User(null, "Bob Grey", "bob@gmail.com");
 
-        Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", maria);
-        Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei Feliz hoje!", maria);
-        
         userRepository.saveAll(Arrays.asList(maria, alex, bob));
+        
+        Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
+        Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei Feliz hoje!", new AuthorDTO(maria));
+    
         postRepository.saveAll(Arrays.asList(post1, post2));
     }
 }
