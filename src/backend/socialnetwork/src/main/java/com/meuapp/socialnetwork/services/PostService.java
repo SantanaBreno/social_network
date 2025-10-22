50818fd7aeffb863e4a54d9d35d5c86103a4924c
Breno Santana <breno.lima@sou.inteli.edu.br>
2025-10-22 08:56:02 -0300

    feat: get post by title

--- src/backend/socialnetwork/src/main/java/com/meuapp/socialnetwork/services/PostService.java
+++ src/backend/socialnetwork/src/main/java/com/meuapp/socialnetwork/services/PostService.java
@@ -1,5 +1,7 @@
 package com.meuapp.socialnetwork.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,5 +18,9 @@ public class PostService {
     public Post findById(String id) {
         return repo.findById(id)
             .orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+
+    public List<Post> findByTitle(String text) {
+        return repo.findByTitleContaining(text);
     }
 }
